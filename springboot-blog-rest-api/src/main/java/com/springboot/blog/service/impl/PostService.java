9980e621b008dbea4e5ef8f681f6b9fd147396bf
Alwayspkr <alwayspawan148@gmail.com>
2022-01-03 17:00:33 +0530

    first commit

---
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/service/impl/PostService.java
@@ -0,0 +1,19 @@
+package com.springboot.blog.service.impl;
+
+import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.payload.PostResponse;
+
+import java.util.List;
+
+public interface PostService {
+
+    PostDto createPost(PostDto postDto);
+
+    PostResponse getAllPosts(int pageNo, int pageSize);
+
+    PostDto getPostById(long id);
+
+    PostDto updatePost(PostDto postDto, long id);
+
+    void deletePostById(long id);
+}
